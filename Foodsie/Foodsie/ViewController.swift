b93cd63edc537d2a8b0469dfd8798931756b41b2
Cons Bulaqueña <consbulaquena@gmail.com>
2018-05-12 18:27:12 +0800

    Add assets, UI login design

---
+++ Foodsie/Foodsie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 12/05/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
