db8c7423cf10087ad415fd3678d3089d04a3e4e6
Cons Bulaqueña <consbulaquena@gmail.com>
2018-05-18 06:52:48 +0800

    Fixed menu toggle

---
+++ Foodsie/Foodsie/RestaurantListingTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantListingTableViewController.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 18/05/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantListingTableViewController: UITableViewController
+{
+    @IBOutlet weak var menuBarButtonItem: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBarButtonItem.target = self.revealViewController()
+        menuBarButtonItem.action = #selector(SWRevealViewController.revealToggle(_: ))
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+    }
+    
+}
