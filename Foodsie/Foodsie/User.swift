c557ecce2b2c418dfe5da0007929864c48736cfc
Cons Bulaqueña <consbulaquena@gmail.com>
2018-06-02 19:37:42 +0800

    Set up User

---
+++ Foodsie/Foodsie/User.swift
@@ -0,0 +1,41 @@
+//
+//  User.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 02/06/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class User
+{
+    var name: String?
+    var email: String?
+    var id: String?
+    var pictureURL: String?
+    
+    static let current = User()
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
