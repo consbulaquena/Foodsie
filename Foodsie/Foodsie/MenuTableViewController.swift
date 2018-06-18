e1d08838f65928575ff7440772c85fa7d12fa329
Cons Bulaqueña <consbulaquena@gmail.com>
2018-06-18 20:38:49 +0800

    Implementing Alamofire

--- Foodsie/Foodsie/MenuTableViewController.swift
+++ Foodsie/Foodsie/MenuTableViewController.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 import Firebase
+import Alamofire
 
 class MenuTableViewController : UITableViewController
 {
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        usernameLabel.text = User.current.name
+        
+        //implem. Alamofire
+        if let pictureURLString = User.current.pictureURL, let url = URL(string: pictureURLString){
+            Alamofire.request(url).responseData { (data) in
+                DispatchQueue.main.async {
+                    self.avatarImageView.image = UIImage(data: data.data)
+                }
+            }
+        }
+    
+    }
     //Before the segue happening
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowLoginViewController" {
             FBLoginManager.shared.logOut()
             try! Auth.auth().signOut()
+            User.current.resetData()
+            
+            
             
         }
     }
