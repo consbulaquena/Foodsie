8b6bbfa9c6c4bad5764f29690ca4c733bfd46389
Cons Bulaqueña <consbulaquena@gmail.com>
2018-07-05 06:02:08 +0800

    Add connections to storyboard

---
+++ Foodsie/Foodsie/RestaurantCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantCell.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 04/07/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class RestaurantCell : UITableViewCell
+ 
+    
+{
+    
+        @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+
+    
+}
+
+
+
