e3d69d5a473b9570bd071eb9db55d21fffdaf7c3
Cons Bulaqueña <consbulaquena@gmail.com>
2018-07-06 17:44:15 +0800

    func updateUI

--- Foodsie/Foodsie/RestaurantCell.swift
+++ Foodsie/Foodsie/RestaurantCell.swift
@@ -10,16 +10,28 @@ import UIKit
 import Alamofire
 
 class RestaurantCell : UITableViewCell
- 
-    
 {
-    
-        @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var restoTypeLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     
-
+    var restaurant: Restaurant! {
+        didSet {
+            self.updateUI()
+        }
+    }
+    
+    func updateUI()
+    {
+        self.addressLabel.text = restaurant.address
+        self.nameLabel.text = restaurant.name
+        self.logoImageView.image = nil
+        
+        
+    }
     
 }
 
