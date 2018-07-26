0a37bfb213aef8f3d8910c9c74914dc55b12d840
Cons Bulaqueña <consbulaquena@gmail.com>
2018-07-27 02:23:19 +0800

    Create Data model class

---
+++ Foodsie/Foodsie/Meal.swift
@@ -0,0 +1,31 @@
+//
+//  Meal.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 27/07/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Meal
+{
+    var id: String?
+    var description: String!
+    var name: String?
+    var imageURL: String?
+    var price: Double?
+    
+    init(json: JSON)
+    {
+        id = json["id"].string
+        name = json["name"].string
+        price = json["price"].double
+        description = json["description"]
+        imageURL = json["imageURL"]
+        
+    }
+    
+    
+}
