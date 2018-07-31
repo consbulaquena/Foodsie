b2a522c4b84723827a6d283440963bb638a2369c
Cons Bulaqueña <consbulaquena@gmail.com>
2018-08-01 03:11:55 +0800

    Add json

---
+++ Foodsie/Foodsie/order.swift
@@ -0,0 +1,59 @@
+//
+//  order.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 01/08/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import Firebase
+
+
+class Order
+{
+    //1 properties
+    //Stripe give token each transaction
+    var stripeToken: String?
+    var restaurantId: String?
+    var orderDetails: [JSON]?
+    var orderDetailDictionaries: [[String: Any]]?
+    var address: String?
+    
+    
+    
+
+    
+    //2 initializers
+        // json if we download frm firebase
+        // when dL data from stripe
+    
+    
+    init(json: JSON)
+    {
+        stripeToken = json["stripeToken"].string
+        restaurantId = json["restaurantId"].string
+        address = json["address"].string
+        orderDetails = json["orderDetails"].array
+        
+        //hmmm
+    }
+    
+        // locally initializer
+    init(stripeToken: String, restaurantId: String, orderDetails: [[String: Any]], address: String)
+    {
+        self.stripeToken = stripeToken
+        self.restaurantId = restaurantId
+        self.orderDetailDictionaries = orderDetails
+        self.address = address
+    }
+    
+    // create method, save to firebase
+    
+    
+    //3. get latest order
+    
+    
+}
