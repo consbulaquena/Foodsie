08a2124f306e4c042b039f07c84904a6bec6095d
Cons Bulaqueña <consbulaquena@gmail.com>
2018-08-04 05:55:43 +0800

    func toggleEmptyCart

---
+++ CartViewController.swift
@@ -0,0 +1,102 @@
+//
+//  CartViewController.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 29/05/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+// TO DO
+// [] outlets / connections
+// [] if there's no item in cart, tell userd do some browsing
+// [] populate the current cart's items into table view
+// [] enter shipping address
+// [] get user's current location via corelocation
+// [] interpret user's shipping address, put pin in map
+
+
+import UIKit
+import MapKit
+
+class CartViewController : UIViewController
+{
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var menuBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var totalView: UIView!
+    @IBOutlet weak var cartTotalLabel: UILabel!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addPaymentButton: UIButton!
+    @IBOutlet weak var addressFieldView: UIView!
+    
+    
+    var cartItems = [CartItem]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Menu code
+        menuBarButtonItem.target = self.revealViewController()
+        menuBarButtonItem.action = #selector(SWRevealViewController.revealToggle(_: ))
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+
+        
+        tableView.dataSource = self
+        
+        if Cart.currentCart.items.count == 0 {
+
+            toggleEmptyCart(isHidden: true)
+            
+            // tehre's no item here.
+            // if there's no item in cart, tell the user to do some browsing
+            let emptyCartLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 80))
+            emptyCartLabel.center = self.view.center
+            emptyCartLabel.textAlignment = .center
+            emptyCartLabel.text = "Your cart is empty.\n Please add some meals."
+            emptyCartLabel.numberOfLines = 3
+            
+            self.view.addSubview(emptyCartLabel)
+        } else {
+            // display all the items in cart
+            toggleEmptyCart(isHidden: false)
+            getMealsInCart()
+        }
+    }
+    
+    func toggleEmptyCart(isHidden: Bool)
+    {
+        tableView.isHidden = isHidden
+        totalView.isHidden = isHidden
+        addressFieldView.isHidden = isHidden
+        addPaymentButton.isHidden = isHidden
+    }
+    
+    func getMealsInCart()
+    {
+        cartItems = Cart.currentCart.items
+        tableView.reloadData()
+        cartTotalLabel.text = "Php\(Cart.currentCart.getTotal())"
+    }
+    
+        
+        
+}
+
+//MARK: - UITableViewDataSource
+
+extension CartViewController : UITableViewDataSource
+{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cartItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CartItemCell", for: indexPath)
+        return cell
+    }
+}
