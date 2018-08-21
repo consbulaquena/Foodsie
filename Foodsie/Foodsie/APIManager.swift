d69a5cce8e6ee685240d5456884e24ab2dfb7705
Cons Bulaqueña <consbulaquena@gmail.com>
2018-08-21 15:52:53 +0800

    Add Stripe secret keys

---
+++ Foodsie/Foodsie/APIManager.swift
@@ -0,0 +1,42 @@
+//
+//  APIManager.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 21/08/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import Foundation
+import Stripe
+import Alamofire
+import SwiftyJSON
+
+class APIManager
+{
+    static let shared = APIManager()
+    var baseURLString: String? = 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Foodsie/Foodsie/Keys.swift
@@ -0,0 +1,14 @@
+//
+//  Keys.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 21/08/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import Foundation
+
+public let STRIPE_PUBLISH_KEY = ""
+public let STRIPE_SECRET_KEY = ""
+public let BACKEND_BASE_URL = "https://warm-retreat-34144.herokuapp.com/"
+
