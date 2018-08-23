e6bf2b4bc90d164291474676c7f6857bea2f9d34
Cons Bulaqueña <consbulaquena@gmail.com>
2018-08-23 21:33:04 +0800

    Set PaymentViewController

---
+++ Foodsie/Foodsie/PaymentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PaymentViewController.swift
+//  Foodsie
+//
+//  Created by Cons Bulaqueña on 23/08/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+import Stripe
+import SwiftyJSON
+
+class PaymentViewController : UIViewController
+{
+    @IBOutlet weak var cardTextField: STPPaymentCardTextField!
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
